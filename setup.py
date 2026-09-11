2895222de7b8b52a2bd1b70aa3d89667e83aa2d4
Tdharma7 <dharmatharun98@gmail.com>
2026-09-11 19:53:49 -0400

    project structure ready,set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tarun",
+    author_email="dharmatharun98@gmail.com",
+    packages=find_packages()
+)
